c1bad341c596198b004d18365ae1b4f5b0647f3c
matt mcgurn <matt@mcgurn.dev>
2021-04-05 19:25:03 -0400

    maybe working version

--- eulerFlow.c
+++ eulerFlow.c
@@ -4,11 +4,219 @@
 
 #define DIM 2                   /* Geometric dimension */
 
+typedef struct {
+    PetscReal gamma;
+    PetscReal rhoL;
+    PetscReal rhoR;
+    PetscReal uL;
+    PetscReal uR;
+    PetscReal pL;
+    PetscReal pR;
+    PetscReal maxTime;
+    PetscReal length;
+} InitialConditions;
+
+typedef struct {
+    PetscReal rho;
+    PetscReal rhoU[2];//Hardcode for 2 for now
+    PetscReal rhoE;
+} EulerNode;
+
+typedef struct {
+    PetscReal pstar,ustar,rhostarL,astarL,SL,SHL,STL,rhostarR,astarR,SR,SHR,STR,gamm1,gamp1;
+} StarState;
+
+
 
 typedef struct {
     InitialConditions initialConditions;
     StarState starState;
 } ProblemSetup;
+
+
+typedef struct{
+    PetscReal f;
+    PetscReal fprm;
+} PressureFunction;
+
+static PressureFunction f_and_fprm_rarefaction(PetscReal pstar, PetscReal pLR, PetscReal aLR, PetscReal gam, PetscReal gamm1,PetscReal gamp1) {
+    // compute value of pressure function for rarefaction
+    PressureFunction function;
+    function.f = ((2. * aLR) / gamm1) * (pow(pstar / pLR, 0.5 * gamm1 / gam) - 1.);
+    function.fprm = (aLR / pLR / gam) * pow(pstar / pLR, -0.5 * gamp1 / gam);
+    return function;
+}
+
+static PressureFunction f_and_fprm_shock(PetscReal pstar, PetscReal pLR, PetscReal rhoLR, PetscReal gam, PetscReal gamm1, PetscReal gamp1){
+    // compute value of pressure function for shock
+    PetscReal A = 2./gamp1/rhoLR;
+    PetscReal B = gamm1*pLR/gamp1;
+    PetscReal sqrtterm =  PetscSqrtReal(A/(pstar+B));
+    PressureFunction function;
+    function.f = (pstar-pLR)*sqrtterm;
+    function.fprm = sqrtterm*(1.-0.5*(pstar-pLR)/(B+pstar));
+    return function;
+}
+
+#define EPS 1.e-6
+#define MAXIT 100
+PetscErrorCode DetermineStarState(const InitialConditions* setup, StarState* starState){
+    // compute the speed of sound
+    PetscReal aL = PetscSqrtReal(setup->gamma*setup->pL/setup->rhoL);
+    PetscReal aR = PetscSqrtReal(setup->gamma*setup->pR/setup->rhoR);
+
+    //first guess pstar based on two-rarefacation approximation
+    starState->pstar = aL+aR - 0.5*(setup->gamma-1.)*(setup->uR-setup->uL);
+    starState->pstar  = starState->pstar  / (aL/pow(setup->pL,0.5*(setup->gamma-1.)/setup->gamma) + aR/pow(setup->pR,0.5*(setup->gamma-1.)/setup->gamma) );
+    starState->pstar = pow(starState->pstar,2.*setup->gamma/(setup->gamma-1.));
+    starState->gamm1 = setup->gamma-1.;
+    starState->gamp1 = setup->gamma+1.;
+
+    PressureFunction fL;
+    if (starState->pstar <= setup->pL){
+        fL = f_and_fprm_rarefaction(starState->pstar, setup->pL,aL,setup->gamma,starState->gamm1,starState->gamp1);
+    }else{
+        fL = f_and_fprm_shock(starState->pstar,setup->pL,setup->rhoL,setup->gamma,starState->gamm1,starState->gamp1);
+    }
+
+    PressureFunction fR;
+    if (starState->pstar <= setup->pR) {
+        fR = f_and_fprm_rarefaction(starState->pstar, setup->pR, aR, setup->gamma, starState->gamm1, starState->gamp1);
+    }else {
+        fR = f_and_fprm_shock(starState->pstar, setup->pR, setup->rhoR, setup->gamma, starState->gamm1, starState->gamp1);
+    }
+    PetscReal delu = setup->uR-setup->uL;
+
+    // iterate using Newton-Rapson
+    if ((fL.f+fR.f+delu)> EPS) {
+        // iterate using Newton-Rapson
+        for(PetscInt it =0; it < MAXIT+4; it++){
+            PetscReal pold = starState->pstar;
+            starState->pstar = pold - (fL.f+fR.f+delu)/(fL.fprm+fR.fprm);
+
+            if(starState->pstar < 0){
+                starState->pstar = EPS;
+            }
+
+            if(2.0*PetscAbsReal(starState->pstar - pold)/(starState->pstar + pold) < EPS){
+                break;
+            }else{
+                if(starState->pstar < setup->pL){
+                    fL = f_and_fprm_rarefaction(starState->pstar, setup->pL,aL,setup->gamma,starState->gamm1,starState->gamp1);
+                }else{
+                    fL = f_and_fprm_shock(starState->pstar,setup->pL,setup->rhoL,setup->gamma,starState->gamm1,starState->gamp1);
+                }
+                if (starState->pstar<=setup->pR) {
+                    fR = f_and_fprm_rarefaction(starState->pstar, setup->pR, aR, setup->gamma, starState->gamm1, starState->gamp1);
+                }else {
+                    fR = f_and_fprm_shock(starState->pstar, setup->pR, setup->rhoR, setup->gamma, starState->gamm1, starState->gamp1);
+                }
+            }
+
+            if (it>MAXIT){
+                SETERRQ(PETSC_COMM_WORLD,1,"error in Riemann.find_pstar - did not converage for pstar" );
+            }
+        }
+    }
+
+    // determine rest of star state
+    starState->ustar = 0.5*(setup->uL+setup->uR+fR.f-fL.f);
+
+    // left star state
+    PetscReal pratio = starState->pstar/setup->pL;
+    if (starState->pstar<=setup->pL) {  // rarefaction
+        starState->rhostarL = setup->rhoL * PetscPowReal(pratio, 1. / setup->gamma);
+        starState->astarL = aL * PetscPowReal(pratio, 0.5 * starState->gamm1 / setup->gamma);
+        starState->SHL = setup->uL - aL;
+        starState->STL = starState->ustar - starState->astarL;
+    }else {  // #shock
+        starState->rhostarL = setup->rhoL * (pratio + starState->gamm1 / starState->gamp1) / (starState->gamm1 * pratio / starState->gamp1 + 1.);
+        starState->SL = setup->uL - aL * PetscSqrtReal(0.5 * starState->gamp1 / setup->gamma * pratio + 0.5 * starState->gamm1 / setup->gamma);
+    }
+
+    // right star state
+    pratio = starState->pstar/setup->pR;
+    if (starState->pstar<=setup->pR) {  // # rarefaction
+        starState->rhostarR = setup->rhoR * PetscPowReal(pratio, 1. / setup->gamma);
+        starState->astarR = aR * PetscPowReal(pratio, 0.5 * starState->gamm1 / setup->gamma);
+        starState-> SHR = setup->uR + aR;
+        starState->STR = starState->ustar + starState->astarR;
+    }else {  // shock
+        starState->rhostarR = setup->rhoR * (pratio + starState->gamm1 / starState->gamp1) / (starState->gamm1 * pratio / starState->gamp1 + 1.);
+        starState->SR = setup->uR + aR * PetscSqrtReal(0.5 * starState->gamp1 / setup->gamma * pratio + 0.5 * starState->gamm1 / setup->gamma);
+    }
+    return 0;
+}
+
+void SetExactSolutionAtPoint(PetscInt dim, PetscReal xDt, const InitialConditions* setup, const StarState* starState, EulerNode* uu){
+    PetscReal p;
+    // compute the speed of sound
+    PetscReal aL = PetscSqrtReal(setup->gamma*setup->pL/setup->rhoL);
+    PetscReal aR = PetscSqrtReal(setup->gamma*setup->pR/setup->rhoR);
+
+    for(PetscInt i =0; i < dim; i++){
+        uu->rhoU[i] = 0.0;
+    }
+
+    if (xDt <= starState->ustar) {  //# left of contact surface
+        if (starState->pstar <= setup->pL) {  // # rarefaction
+            if (xDt <= starState->SHL) {
+                uu->rho = setup->rhoL;
+                p = setup->pL;
+                uu->rhoU[0] = setup->uL*uu->rho;
+            }else if (xDt <=starState->STL) {  //#SHL < x / t < STL
+                PetscReal tmp = 2. / starState->gamp1 + (starState->gamm1 / starState->gamp1 / aL) * (setup->uL - xDt);
+                uu->rho = setup->rhoL * pow(tmp, 2. / starState->gamm1);
+                uu->rhoU[0] = uu->rho * (2. / starState->gamp1) * (aL + 0.5 * starState->gamm1 * setup->uL + xDt);
+                p = setup->pL * pow(tmp, 2. * setup->gamma / starState->gamm1);
+            }else {  //# STL < x/t < u*
+                uu->rho = starState->rhostarL;
+                p = starState->pstar;
+                uu->rhoU[0] = uu->rho * starState->ustar;
+            }
+        }else{  //# shock
+            if (xDt<= starState->SL) {  // # xDt < SL
+                uu->rho = setup->rhoL;
+                p = setup->pL;
+                uu->rhoU[0] = uu->rho * setup->uL;
+            }else {  //# SL < xDt < ustar
+                uu->rho = starState->rhostarL;
+                p = starState->pstar;
+                uu->rhoU[0] = uu->rho * starState->ustar;
+            }
+        }
+    }else{//# right of contact surface
+        if (starState->pstar<=setup->pR) {  //# rarefaction
+            if (xDt>= starState->SHR) {
+                uu->rho = setup->rhoR;
+                p = setup->pR;
+                uu->rhoU[0] = uu->rho * setup->uR;
+            }else if (xDt >= starState->STR) {  // # SHR < x/t < SHR
+                PetscReal tmp = 2./starState->gamp1 - (starState->gamm1/starState->gamp1/aR)*(setup->uR-xDt);
+                uu->rho = setup->rhoR*PetscPowReal(tmp,2./starState->gamm1);
+                uu->rhoU[0] = uu->rho * (2./starState->gamp1)*(-aR + 0.5*starState->gamm1*setup->uR+xDt);
+                p = setup->pR*PetscPowReal(tmp,2.*setup->gamma/starState->gamm1);
+            }else{ //# u* < x/t < STR
+                uu->rho = starState->rhostarR;
+                p = starState->pstar;
+                uu->rhoU[0] = uu->rho * starState->ustar;
+            }
+        }else {//# shock
+            if (xDt>= starState->SR) {  // # xDt > SR
+                uu->rho = setup->rhoR;
+                p = setup->pR;
+                uu->rhoU[0] = uu->rho * setup->uR;
+            }else {//#ustar < xDt < SR
+                uu->rho = starState->rhostarR;
+                p = starState->pstar;
+                uu->rhoU[0] = uu->rho * starState->ustar;
+            }
+        }
+    }
+    PetscReal e =  p/starState->gamm1/uu->rho;
+    PetscReal E = e + 0.5*(uu->rhoU[0]/uu->rho)*(uu->rhoU[0]/uu->rho);
+    uu->rhoE = uu->rho*E;
+}
 
 
 static PetscErrorCode SetExactSolutionRho(PetscInt dim, PetscReal time, const PetscReal x[], PetscInt Nf, PetscScalar *u, void *ctx){
@@ -60,8 +268,8 @@ static PetscErrorCode PrintVector(DM dm, Vec v, PetscInt step, const char * file
 
     PetscInt rank = 0;
     PetscInt size;
-    ierr = MPI_Comm_rank(PetscObjectComm(dm), &rank);CHKERRMPI(ierr);
-    ierr = MPI_Comm_size(PetscObjectComm(dm), &size);CHKERRMPI(ierr);
+    ierr = MPI_Comm_rank(PetscObjectComm((PetscObject)dm), &rank);CHKERRMPI(ierr);
+    ierr = MPI_Comm_size(PetscObjectComm((PetscObject)dm), &size);CHKERRMPI(ierr);
 
     for(PetscInt r =0; r < size; r++ ) {
         if(r == rank) {
@@ -88,7 +296,7 @@ static PetscErrorCode PrintVector(DM dm, Vec v, PetscInt step, const char * file
 
             fclose(fptr);
         }
-        MPI_Barrier(PetscObjectComm(dm));
+        MPI_Barrier(PetscObjectComm((PetscObject)dm));
     }
     ierr = VecRestoreArrayRead(cellgeom, &cgeom);CHKERRQ(ierr);
     ierr = VecRestoreArrayRead(v, &x);CHKERRQ(ierr);
@@ -109,7 +317,7 @@ static PetscErrorCode MonitorError(TS ts, PetscInt step, PetscReal time, Vec u, 
     ierr = PetscObjectSetName((PetscObject)e, "exact");CHKERRQ(ierr);
 
     // Set the values
-    PetscErrorCode     (*func[3]) (PetscInt dim, PetscReal time, const PetscReal x[], PetscInt Nf, PetscScalar *u, void *ctx) = {{SetExactSolutionRho, SetExactSolutionRhoU, SetExactSolutionRhoE}};
+    PetscErrorCode     (*func[3]) (PetscInt dim, PetscReal time, const PetscReal x[], PetscInt Nf, PetscScalar *u, void *ctx) = {SetExactSolutionRho, SetExactSolutionRhoU, SetExactSolutionRhoE};
     void* ctxs[3] ={ctx, ctx, ctx};
     ierr    = DMProjectFunction(dm,time,func,ctxs,INSERT_ALL_VALUES,e);CHKERRQ(ierr);
 
@@ -183,7 +391,7 @@ int main(int argc, char **argv)
     // Create a ts
     ierr = TSCreate(PETSC_COMM_WORLD, &ts);CHKERRQ(ierr);
     ierr = TSSetProblemType(ts, TS_NONLINEAR);CHKERRQ(ierr);
-    ierr = TSSetType(ts, TSEULER);CHKERRQ(ierr);
+    ierr = TSSetType(ts, TSRK);CHKERRQ(ierr);
     ierr = TSSetExactFinalTime(ts, TS_EXACTFINALTIME_MATCHSTEP);CHKERRQ(ierr);
 
     // Create a mesh
@@ -204,9 +412,20 @@ int main(int argc, char **argv)
     problem.initialConditions.rhoR=0.125;
     problem.initialConditions.uR=0.0;
     problem.initialConditions.pR=0.1;
-    problem.initialConditions.maxTime = 1.0;
+    problem.initialConditions.maxTime = 0.25;
     problem.initialConditions.length = 1;
     problem.initialConditions.gamma = 1.4;
+
+    // case 2
+//    problem.initialConditions.rhoL=1.0;
+//    problem.initialConditions.uL=-2.0;
+//    problem.initialConditions.pL=0.4;
+//    problem.initialConditions.rhoR=1.0;
+//    problem.initialConditions.uR=2.0;
+//    problem.initialConditions.pR=0.4;
+//    problem.initialConditions.maxTime = 0.15;
+//    problem.initialConditions.length = 1;
+//    problem.initialConditions.gamma = 1.4;
 
     // Compute the star state
     ierr = DetermineStarState(&problem.initialConditions, &problem.starState);CHKERRQ(ierr);
@@ -217,7 +436,7 @@ int main(int argc, char **argv)
 
     // Add in the flow parameters
     EulerFlowParameters flowParam;
-    flowParam.cfl = .5;
+    flowParam.cfl = 1.0;
     flowParam.gamma = problem.initialConditions.gamma;
 
     //Store the flow params
